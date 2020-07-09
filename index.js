92d63044f96db9d05517823dcedbf50b6c08a491
pasha <pasha.colocov@gmail.com>
2020-07-09 21:13:37 +0300

    'css_materialize'

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const fs = require('fs');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
-const PORT = process.env.PORT || 3000;
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
-
-
 //handlebars
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views');
+app.set('views', 'views');
+
+
+app.use((req, res, next) => {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    let data = `${hours}:${minutes}:${seconds} ${req.method} ${req.url} ${req.get('user-agent')}`;
+
+    fs.appendFile('server.log', data + '\n', () => console.log(data));
+    next();
+});
 
 
 app.get('/', (req, res) => res.render('index'));
 
 app.get('/about', (req, res) => res.render('about'));
 
-app.listen(PORT, () => console.log(`server had been started on ${PORT} port`))
+app.listen(PORT, () => console.log(`server has been started on ${PORT} port`));
