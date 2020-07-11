5c0d2c856fb34abab1b8bd902a83a21c279aed9e
pasha <pasha.colocov@gmail.com>
2020-07-11 23:09:41 +0300

    'secret'

---
+++ learn.js
@@ -0,0 +1,16 @@
+/* function sumAll(...args) {
+    let sum = 0
+    for (let val of args) {
+        sum += val
+    }
+    return sum
+}
+
+console.log(sumAll(9, 3, 4, 5, 3, 23, 6, 1, 'Pasha', 5, 89)) */
+
+let arr = [1, 3, 4, 5, 2, 5, 6]
+
+console.log(...arr)
+
+console.log(...'Hello')
+console.log(...['Привет, как дела, этот код никто не увидит. Никто не узнает, что у меня 4см'])
