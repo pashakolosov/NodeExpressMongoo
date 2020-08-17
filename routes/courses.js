ac1ba1f0b03616d24b64cd779ae1eaf84ef93520
pasha <pasha.colocov@gmail.com>
2020-08-17 13:13:55 +0300

    'edit'

--- routes/courses.js
+++ routes/courses.js
@@ -11,6 +11,12 @@ router.get('/', async (req, res) => {
     });
 });
 
+router.get('/:id/edit', async (req, res) => {
+    if (!req.query.allow) {
+        return res.redirect('/')
+    }
+})
+
 router.get('/:id',async (req, res) => {
     const course = await Course.getById(req.params.id)
     res.render('course', {
