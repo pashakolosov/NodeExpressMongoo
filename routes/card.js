9a202b0befd53e1e557ac225527862a52231bce6
pasha <pasha.colocov@gmail.com>
2020-08-18 11:02:37 +0300

    'm'

--- routes/card.js
+++ routes/card.js
@@ -1,8 +1,20 @@
 const {Router} = require('express')
+const Card = require('../models/card')
 const router = Router()
+const Course = require('../models/course')
 
 router.post('/add', async (req, res) => {
-    
+    const course = await Course.getById(req.body.id)
+    await Card.add(course) 
+    res.redirect('/card')
+})
+
+router.get('/', async (req, res) => {
+    const card = await Card.fetch()
+    res.render('card', {
+        title: 'Card',
+        card
+    })
 })
 
 module.exports = router
