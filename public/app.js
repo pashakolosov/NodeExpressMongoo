f7d6f0cf0ea92101fdd210e070ed86d221e794a8
pasha <pasha.colocov@gmail.com>
2020-08-18 17:24:01 +0300

    'finish'

--- public/app.js
+++ public/app.js
@@ -1,8 +1,13 @@
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-RU', {
+        currency: 'rub',
+        style: 'currency'
+      }).format(price)
+
+}
+
 document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('ru-RU', {
-      currency: 'rub',
-      style: 'currency'
-    }).format(node.textContent)
+    node.textContent = toCurrency(node.textContent)
   })
   
   const $card = document.querySelector('#card')
@@ -15,7 +20,23 @@ document.querySelectorAll('.price').forEach(node => {
           method: 'delete'
         }).then(res => res.json())
           .then(card => {
-            console.log(card)
+            if (card.courses.length) {
+                const html = card.courses.map(c => {
+                    return `
+                    <tr>
+                    <td>${c.title}</td>
+                    <td>${c.count}</td>
+                    <td>
+                        <button class="btn btn-small js-remove" data-id="${c.id}">Delete</button>
+                    </td>
+                </tr>
+                    `
+                }).join('')
+                $card.querySelector('tbody').innerHTML = html
+                $card.querySelector('.price').textContent = toCurrency(card.price)
+            } else {
+                $card.innerHTML = '<p>Card is empty</p>'
+            }
           })
       }
       
